dd724029cc1df64ca4ff1df89bdf254451a0c397
shiuns <shiunworks@gmail.com>
2022-10-23 22:42:59 +0800

    加入專案檔案。

---
+++ [作業(Exec_While_Q1 )]-21謝孟勳/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_While_Q1____21謝孟勳
+{
+    internal class Program
+    {
+        //1+ 2 + 3 + 4 + .... + N <105
+        //請問N是多少?
+        static void Main(string[] args)
+        {
+            int number = 1;
+            int sum = 0;
+            while (true)
+            { 
+                sum += number;
+                if (sum >= 190 )
+                    break;
+                number++;
+            }
+            Console.WriteLine(number-1);
+        }
+    }
+}
